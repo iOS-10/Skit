8a4a144a957f50bddca3322af6bbe6362cbe15a7
HardcoreIOS <Abdurrahman@AbdurrahmansMBP.home>
2017-01-31 13:51:36 -0500

    Created Login/Signup UI

---
+++ Skit/SignInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skit
+//
+//  Created by Abdurrahman on 1/31/17.
+//  Copyright © 2017 AR Ehsan. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+
+}
+
